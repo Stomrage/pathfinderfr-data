bb9ee8103de6e479430adc38265fe80b9d7f9a50
Stomrage <theo.coulon@gmail.com>
2019-10-05 13:11:16 +0200

    new utility function added to the parser_content and changed the class_feat_parser to use those utility functions

--- scraping/parser/parser_content.py
+++ scraping/parser/parser_content.py
@@ -1,7 +1,10 @@
 import re
+from functools import reduce
 
-from typing import Pattern
+from typing import Pattern, List
 from bs4 import BeautifulSoup
+
+from scraping.main import libhtml
 
 
 class ParserContent:
@@ -11,8 +14,27 @@ class ParserContent:
 
     def select_and_filter_one_by_regex(self, selector: str, regex: Pattern) -> BeautifulSoup:
         filter_results = self.select_and_filter_by_regex(selector, regex)
-        assert(len(filter_results) == 1)
+        assert (len(filter_results) == 1)
         return filter_results[0]
 
-    def select_and_filter_by_regex(self, selector: str, regex: Pattern) -> list:
+    def select_and_filter_by_regex(self, selector: str, regex: Pattern) -> List:
         return list(filter(lambda x: regex.match(x.text), self.content.select(selector)))
+
+    def select_and_extract_text_until(self, selector: str, tag_name: str):
+        selected_map = {}
+        for selected_bs in self.content.select(selector):
+            selected_map[self.__clean_title(selected_bs.text)] = reduce(lambda a,b: a+libhtml.html2text(b).replace("\n", ""), self.__extract_siblings_until_tag(tag_name, selected_bs), "")
+        return selected_map
+
+    @staticmethod
+    def __clean_title(title_text: str):
+        return re.sub("¶|\.|\*|\(Ext\)|\(Sur\)|\(Mag\)", "", title_text).strip()
+
+    @staticmethod
+    def __extract_siblings_until_tag(tag_name: str, current_bs: BeautifulSoup):
+        extracted_siblings = []
+        for sibling in current_bs.next_siblings:
+            if sibling.name == tag_name:
+                break
+            extracted_siblings.append(sibling)
+        return extracted_siblings
--- scraping/parser/specialized/class_feat_parser_holder.py
+++ scraping/parser/specialized/class_feat_parser_holder.py
@@ -1,29 +1,25 @@
 import re
+from typing import List
+
+from bs4 import BeautifulSoup
 
 from scraping.parser.parser_content import ParserContent
 from scraping.model.holder import Holder
-from scraping.model.talent import Talent
+from scraping.model.classfeat import ClassFeat
 from scraping.parser.parser_for_holder import ParserForHolder
 from scraping.util import data_util
 from scraping.main import libhtml
+from scraping.util.data_util import ComparisonMethod
 
 
 class ClassFeatParserHolder(ParserForHolder):
-
     class_name: str = None
 
-    @staticmethod
-    def __normalize_talent_name(talent_name):
-        talent_name = re.sub(r" \(.*\)\.?", "", talent_name)
-        talent_name = talent_name.replace("-", " ")
-        talent_name = talent_name.replace("’", "'")
-        return talent_name
-
     def create_data(self) -> Holder:
-        return Talent()
+        return ClassFeat()
 
     def parse_title(self, title_text: str):
-        class_name = (data_util.verify_class(title_text))
+        class_name = (data_util.verify_class(title_text, ComparisonMethod.INSIDE))
         if class_name is not None:
             self.class_name = class_name
 
@@ -31,15 +27,8 @@ class ClassFeatParserHolder(ParserForHolder):
         return self.class_name
 
     def parse_html(self, data_content: ParserContent):
-        talents_description = data_content.content.select_one("div.article_2col")
-        talent_name = None
-        talent_text = None
-        for talent_description in talents_description.children:
-            if talent_description.name == "h3":
-                if talent_name is not None:
-                    self._create_or_retrieve(talent_name).talent_description = talent_text
-                talent_name = libhtml.cleanSectionName(talent_description.get_text())
-                talent_name = self.__normalize_talent_name(talent_name)
-                talent_text = ""
-            elif talent_name is not None:
-                talent_text += (libhtml.html2text(talent_description)).replace("\n", "")
+        class_feat_map = data_content.select_and_extract_text_until(selector="h3.separator", tag_name="h3")
+        for class_feat_key in class_feat_map.keys():
+            class_feat: ClassFeat = self._create_or_retrieve(class_feat_key)
+            class_feat.class_name = self.class_name
+            class_feat.class_feat_description = class_feat_map[class_feat_key]
