3602fe2aea28ba780f4583722f3da7e5bc76453e
Stomrage <theo.coulon@gmail.com>
2019-10-05 13:08:40 +0200

    New function to directly access the file

--- data/data_accessor.py
+++ data/data_accessor.py
@@ -7,6 +7,12 @@ import yaml
 logger = logging.getLogger("MockAccessor")
 
 
+def get_data_file(filename: str):
+    logger.debug("Accessing data file : {filename}".format(filename=filename))
+    holder_path = Path(__file__).parent
+    return str(holder_path.joinpath(filename).open(encoding="utf-8").read())
+
+
 def save_yaml(file_name, datas: List):
     if datas:
         logger.debug("Writing yaml file : {file_name}".format(file_name=file_name))
